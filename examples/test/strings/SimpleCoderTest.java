643d247f4d8f28fb207bd058c53d1fff4b00d78d
iproduct <office@iproduct.org>
2014-10-25 12:19:05 +0200

    SimpleCoder example + unit test.

---
+++ examples/test/strings/SimpleCoderTest.java
@@ -0,0 +1,19 @@
+package strings;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleCoderTest {
+
+	@Test
+	public void testEncode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDecode() {
+		fail("Not yet implemented");
+	}
+
+}
