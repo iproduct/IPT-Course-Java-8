378ac1d28425de79a284cedc6082cbdadf5d7a71
iproduct <office@iproduct.org>
2014-12-22 20:14:06 +0200

    java_io project added

--- eShop/src/lambdas/HelloLabmda.java
+++ eShop/src/lambdas/HelloLabmda.java
@@ -1,8 +1,14 @@
 package lambdas;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import eshop.entity.Item;
 
@@ -21,18 +27,29 @@ public class HelloLabmda {
 					"Good archiving solution for small office", 125.7, 15),
 
 			new Item(9L, "Motherboard", "ASUS", "Motherboards",
-					"X99-SOC Force", 125.7, 15),
+					"X99-SOC Force motherboard", 125.7, 15),
 
 	};
 
 	public static void main(String[] args) {
 		List<Item> il = Arrays.asList(items);
-		il.stream().forEachOrdered(s -> System.out.println(s));
+		il.stream().sorted(Comparator.comparing(Item::getName).reversed())
+			.forEachOrdered(System.out::println);	
+		System.out.println(
+			il.stream()
+			.mapToDouble((Item i) -> i.getStockQuantity() * i.getPrice())
+			.sum());
 
 		// Old way:
 		System.out.println("\nOld way:");
 		List<Integer> list = Arrays.asList(4, 3, 6, 7, 2, 5, 1);
-		for (Integer n : list) {
+		Collections.sort(list, new Comparator<Integer>() {
+			@Override
+			public int compare(Integer i1, Integer i2) {
+				return i2 - i1;
+			}
+		});
+		for (Integer n : list) {	
 			int x = n * n;
 			System.out.println(x);
 		}
@@ -40,8 +57,11 @@ public class HelloLabmda {
 		// New way:
 		System.out.println("\nJava 8 way:");
 		List<Integer> list1 = Arrays.asList(4, 3, 6, 7, 2, 5, 1);
-		list.stream().sorted((i, j) -> j - i).map((x) -> x * x)
-				.forEach(System.out::println);
+		Consumer<Integer> p = System.out::println;	
+		
+		list1.stream().sorted((i1, i2) -> i2-i1 )
+			.map(x -> x*x)
+			.forEach(p);
 
 		// Old way:
 		System.out.println("\nOld way:");
@@ -53,11 +73,27 @@ public class HelloLabmda {
 		}
 		System.out.println(sum2);
 
-		// New way:
+//		// New way:
 		System.out.println("\nJava 8 way:");
 		List<Integer> list3 = Arrays.asList(4, 3, 6, 7, 2, 5, 1);
-		int sum3 = list3.stream().map(x -> x * x).reduce((x, y) -> x + y).get();
+		int sum3 = list3.stream().sorted().map(x -> x * x)
+				.reduce((x, y) -> x + y).get();
 		System.out.println(sum3);
+		
+		//Test find keyword
+		System.out.println("============================>");
+		
+		String keywordsRegex = "motherboard";
+		List<Item> itemsList = Arrays.asList(items);
+		Pattern pat = Pattern.compile(keywordsRegex);
+		itemsList.parallelStream().flatMap((Item i) -> {
+			Map<Item, Integer> matches = new HashMap<>();
+			Matcher m = pat.matcher(i.getDescription());
+			while(m.find())
+				matches.put(i, m.start());
+			return matches.entrySet().stream();
+		}).forEach(System.out::println);	
+		
 
 	}
 
