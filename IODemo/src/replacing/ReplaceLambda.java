22d8ee510c1a98b0a95c18a77fb66624e05c3b78
iproduct <office@iproduct.org>
2015-01-11 13:57:11 +0200

    IODemo projetc: Unit test added for ReplaceLambda class.

--- IODemo/src/replacing/ReplaceLambda.java
+++ IODemo/src/replacing/ReplaceLambda.java
@@ -6,19 +6,29 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import static java.nio.file.StandardOpenOption.*;
 
 public class ReplaceLambda {
 
+	public void replaceOccurencesFromInfileToOutfile(
+		String inFileName, String outFileName, String toBeFound, String replacement)
+		throws IOException {
+		List<String> resultLines = 
+			Files.lines(Paths.get(inFileName))
+			.map(s -> s.replaceAll(toBeFound, replacement) )
+			.collect(Collectors.toList());
+		Files.write(Paths.get(outFileName), resultLines);
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		if(args.length != 4) {
 			System.out.println("Usage:\njava Replace <infilename> <outfilename> <word-to-be-replaced> <replacement>");
 			return;
 		}
-		List<String> resultLines = 
-			Files.lines(Paths.get(args[0])).map(s -> s.replaceAll(args[2], args[3]) )
-			.collect(Collectors.toList());
-		Files.write(Paths.get(args[1]), resultLines, APPEND);
+		ReplaceLambda instance = new ReplaceLambda();
+		instance.replaceOccurencesFromInfileToOutfile(args[0], args[1], args[2], args[3]);
 	    System.out.println("Replacement is successful to file: " + args[1]);	  
 	}
+
 }
