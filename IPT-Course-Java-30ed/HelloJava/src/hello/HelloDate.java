f3689ecb2f157c057bfb36635ccb0bb1e2c1ea94
iproduct <office@iproduct.org>
2015-04-19 10:05:01 +0200

    Initial commit for the project. HelloDate class added.

---
+++ IPT-Course-Java-30ed/HelloJava/src/hello/HelloDate.java
@@ -0,0 +1,13 @@
+package hello;
+
+import java.util.Date;
+
+public class HelloDate {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from Java 8 at "
+				+ new Date());
+		//TODO add more
+	}
+
+}
