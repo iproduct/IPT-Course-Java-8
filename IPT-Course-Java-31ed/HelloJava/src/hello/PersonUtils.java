44a9e19fa8c3ef81ba96aa2f81877f2513a597ec
iproduct <office@iproduct.org>
2015-07-05 13:10:27 +0200

    Final validation.

--- IPT-Course-Java-31ed/HelloJava/src/hello/PersonUtils.java
+++ IPT-Course-Java-31ed/HelloJava/src/hello/PersonUtils.java
@@ -14,18 +14,23 @@ public class PersonUtils {
 		
 		//read SSN
 		p.setIdNumber(inputLongNumber("SSN", 10, 10));
-		p.setName(inputString("Name", 5, 100));
+		p.setName(inputString("Name", 5, 100, false));
 		p.setAge((int)inputLongNumber("Age", 1, 3));
+		p.setAddress(inputString("Address", 5, 100, true));
+		p.setPhone(inputString("Phone", 5, 20, true));
 		
 		return p;
 	}
 
-	public static String inputString(String fieldName, int minChars, int maxChars) {
+	public static String inputString(String fieldName, int minChars, int maxChars, 
+			boolean hasDigits) {
 		String line;
 		while(true) {
 			System.out.print("Input your " + fieldName + ": ");
 			line = sc.nextLine();
-			if (Pattern.matches("[a-zA-Z\\s]{" + minChars + "," + maxChars + "}", line)) {
+			if (Pattern.matches("[a-zA-Z"
+					+ (hasDigits ? "\\d\\(\\)\\+" : "") 
+					+ "\\s]{" + minChars + "," + maxChars + "}", line)) {
 				return line.trim();
 			} else {
 				System.err.println("Invalid " + fieldName + ". Try again.");
