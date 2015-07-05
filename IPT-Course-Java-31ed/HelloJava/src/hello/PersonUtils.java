b69ade81b5770201150b9a3feac67c71ccd0de95
iproduct <office@iproduct.org>
2015-07-05 12:56:11 +0200

    Validation added to HelloJava project

--- IPT-Course-Java-31ed/HelloJava/src/hello/PersonUtils.java
+++ IPT-Course-Java-31ed/HelloJava/src/hello/PersonUtils.java
@@ -5,20 +5,50 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class PersonUtils {
+	private static Scanner sc; 
+	
 	public static Person inputPerson(InputStream in){
-		Scanner sc = new Scanner(in);
+		sc = new Scanner(in);
 		Person p = new Person();
 		String line;
-		do {
-			System.out.print("Input your SSN: ");
+		
+		//read SSN
+		p.setIdNumber(inputLongNumber("SSN", 10, 10));
+		p.setName(inputString("Name", 5, 100));
+		p.setAge((int)inputLongNumber("Age", 1, 3));
+		
+		return p;
+	}
+
+	public static String inputString(String fieldName, int minChars, int maxChars) {
+		String line;
+		while(true) {
+			System.out.print("Input your " + fieldName + ": ");
 			line = sc.nextLine();
-//			Pattern.matches("\\d{10}", line)
-		} while(p.getIdNumber() > 0);
-		return null;
+			if (Pattern.matches("[a-zA-Z\\s]{" + minChars + "," + maxChars + "}", line)) {
+				return line.trim();
+			} else {
+				System.err.println("Invalid " + fieldName + ". Try again.");
+			}
+		}
+	}
+
+	public static long inputLongNumber(String fieldName, int minDigits, int maxDigits) {
+		String line;
+		while(true) {
+			System.out.print("Input your " + fieldName + ": ");
+			line = sc.nextLine();
+			if (Pattern.matches("\\d{" + minDigits + "," + maxDigits +  "}", line)) {
+				return Long.parseLong(line);
+			} else {
+				System.err.println("Invalid " + fieldName + ". Try again.");
+			}
+		} 
 	}
 
 	public static void main(String[] args) {
-		
+		Person p = inputPerson(System.in);
+		System.out.println(p);
 
 	}
 
