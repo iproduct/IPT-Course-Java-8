d6bcf002020a86399333d08b1d232b37fb61cda1
iproduct <office@iproduct.org>
2015-07-11 09:59:00 +0200

    HelloJava: ocupation added

--- IPT-Course-Java-31ed/HelloJava/src/hello/HelloDate.java
+++ IPT-Course-Java-31ed/HelloJava/src/hello/HelloDate.java
@@ -1,15 +1,19 @@
 package hello;
 
 import java.util.Date;
+import static hello.Person.*;
 
 public class HelloDate {
 
 	public static void main(String[] args) {
 		Date date = new Date();
 		System.out.println("Today is: " + date);
-		Person p = new Person(750724123L, "Ivan Petrov", 38, "Sofia 1000","(+3592) 973 38 38");
-		Person p2 = new Person(750724123L, "Ivan Petrov", 38, "Sofia 1000","(+3592) 973 38 38");
-		Person p3 = new Person(750724123L, "Ivan Petrov", 38, "Sofia 1000","(+3592) 973 38 38");
+		Person p = new Person(750724123L, "Ivan Petrov", 38, 
+				"Sofia 1000","(+3592) 973 38 38", EMPLOYEE);
+		Person p2 = new Person(750724123L, "Ivan Petrov", 38, 
+				"Sofia 1000","(+3592) 973 38 38", STUDENT);
+		Person p3 = new Person(750724123L, "Ivan Petrov", 38, 
+				"Sofia 1000","(+3592) 973 38 38", SELF_EMPLOYED);
 		System.out.println("SSN:" +  p.getIdNumber());
 //		p.setIdNumber(760724123L);
 		System.out.println("SSN:" +  p.getIdNumber());
--- IPT-Course-Java-31ed/HelloJava/src/hello/Person.java
+++ IPT-Course-Java-31ed/HelloJava/src/hello/Person.java
@@ -1,20 +1,28 @@
 package hello;
 
 public class Person {
+	public static final int NONE = 0;
+	public static final int STUDENT = 1;
+	public static final int EMPLOYEE = 2;
+	public static final int SELF_EMPLOYED = 3;
+	
 	private long idNumber;
 	private String name;
 	private int age;
 	private String address;
 	private String phone;
+	private int ocupation;
 		
 	public Person() {}
 
-	public Person(long id, String name, int age, String address, String phone) {
+	public Person(long id, String name, int age, String address, String phone,
+			int ocupation) {
 		idNumber = id;
 		this.name = name;
 		this.age = age;
 		this.address = address;
 		this.phone = phone;
+		this.ocupation = ocupation;
 	}
 	
 	public long getIdNumber() {
@@ -57,6 +65,14 @@ public class Person {
 		this.phone = phone;
 	}
 
+	public int getOcupation() {
+		return ocupation;
+	}
+
+	public void setOcupation(int ocupation) {
+		this.ocupation = ocupation;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -82,7 +98,8 @@ public class Person {
 	@Override
 	public String toString() {
 		return "Person [idNumber=" + idNumber + ", name=" + name + ", age="
-				+ age + ", address=" + address + ", phone=" + phone + "]";
+				+ age + ", address=" + address + ", phone=" + phone
+				+ ", ocupation=" + ocupation + "]";
 	}
 
 	public static void incrementAgeBy10(Person p) {
