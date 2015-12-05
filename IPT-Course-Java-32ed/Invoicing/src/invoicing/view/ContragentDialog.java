9b0416d6bedf91c0e51195ac1e2e01177554f5ac
iproduct <office@iproduct.org>
2015-12-05 14:12:11 +0200

    Product entry form added to Invoicing project.

---
+++ IPT-Course-Java-32ed/Invoicing/src/invoicing/view/ContragentDialog.java
@@ -0,0 +1,38 @@
+package invoicing.view;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
+import javax.swing.JSeparator;
+
+public class ContragentDialog extends JDialog {
+	public ContragentDialog() {
+		
+		JLabel label = new JLabel("");
+		getContentPane().add(label, BorderLayout.NORTH);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmOpen = new JMenuItem("Open");
+		mntmOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_MASK));
+		mnFile.add(mntmOpen);
+		
+		JSeparator separator = new JSeparator();
+		mnFile.add(separator);
+		
+		JMenuItem mntmSave = new JMenuItem("Save");
+		mntmSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_MASK));
+		mnFile.add(mntmSave);
+	}
+
+}
