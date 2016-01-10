4427c80cddeac720f654122cb51f56f5174a31e6
iproduct <office@iproduct.org>
2016-01-10 10:52:36 +0200

    GUI-net interfaces added

--- IPT-Course-Java-32ed/networking/src/chat/NetClient.java
+++ IPT-Course-Java-32ed/networking/src/chat/NetClient.java
@@ -1,5 +1,11 @@
 package chat;
 
+import chat.model.ConnectionSettings;
+
 public interface NetClient {
 	String login(ConnectionSettings settings);
+	boolean sendMessage(String message);
+	void logout();
+	void addMessageListener(MessageListener ml);
+	void removeMessageListener(MessageListener ml);
 }
