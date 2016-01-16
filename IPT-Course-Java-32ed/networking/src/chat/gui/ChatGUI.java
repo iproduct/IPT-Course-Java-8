b848de9bc8ee3fea53687b9c27dce0ae198cebf5
iproduct <office@iproduct.org>
2016-01-16 16:19:22 +0200

    ChatGUI send message.

--- IPT-Course-Java-32ed/networking/src/chat/gui/ChatGUI.java
+++ IPT-Course-Java-32ed/networking/src/chat/gui/ChatGUI.java
@@ -4,22 +4,33 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
 import javax.swing.KeyStroke;
 
 import chat.NetClient;
 import chat.client.TCPChatClient;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class ChatGUI {
 
 	private JFrame frame;
 	private SignInDialog signDialog;
 	private NetClient netClient;
+	private JTextField jtfNewMessage;
 
 	/**
 	 * Launch the application.
@@ -54,7 +65,7 @@ public class ChatGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 600);
+		frame.setBounds(100, 100, 500, 600);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -93,6 +104,73 @@ public class ChatGUI {
 		
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
+		
+		JTextArea taMessages = new JTextArea();
+		taMessages.setEditable(false);
+		JScrollPane textArea = new JScrollPane(taMessages);
+		
+		JLabel lblMessages = new JLabel("Messages:");
+		
+		jtfNewMessage = new JTextField();
+		jtfNewMessage.setColumns(10);
+		jtfNewMessage.addKeyListener(new KeyAdapter() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (e.getKeyCode()==KeyEvent.VK_ENTER){
+					sendMessage();
+		        }	
+			}
+		});
+		
+		JLabel lblNewMessage = new JLabel("New message:");
+		
+		JButton btnNewButton = new JButton("Send Message");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				sendMessage();
+			}
+		});
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 764, Short.MAX_VALUE)
+								.addComponent(lblMessages)
+								.addComponent(lblNewMessage)
+								.addComponent(jtfNewMessage, GroupLayout.DEFAULT_SIZE, 764, Short.MAX_VALUE)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(331)
+							.addComponent(btnNewButton)))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(26)
+					.addComponent(lblMessages)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 268, GroupLayout.PREFERRED_SIZE)
+					.addGap(27)
+					.addComponent(lblNewMessage)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(jtfNewMessage, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(52)
+					.addComponent(btnNewButton)
+					.addContainerGap(75, Short.MAX_VALUE))
+		);
+		frame.getContentPane().setLayout(groupLayout);
 	}
 
+	private void sendMessage() {
+		String message = jtfNewMessage.getText();
+		if(message.length() > 0) {
+			netClient.sendMessage(jtfNewMessage.getText());
+			jtfNewMessage.setText("");
+		}
+	}
 }
