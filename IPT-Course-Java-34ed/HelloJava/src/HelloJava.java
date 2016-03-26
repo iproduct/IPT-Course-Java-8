504990b1785d58721f5eb9a7245231c068a601f1
iproduct <office@iproduct.org>
2016-03-26 13:22:38 +0200

    Person class + javadoc sample added.

--- IPT-Course-Java-34ed/HelloJava/src/HelloJava.java
+++ IPT-Course-Java-34ed/HelloJava/src/HelloJava.java
@@ -8,9 +8,32 @@ public class HelloJava {
 		System.out.println(now + ": Hello from Java 8!");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("What's your name?");
-		String name = sc.nextLine();
-		System.out.println("Hello " + name + ", from Java 8!");
-
+//		String name = sc.nextLine();
+//		System.out.println("Hello " + name + ", from Java 8!");
+		
+		String myName;
+		myName = new String("Ivan Petrov");
+		System.out.println(myName);
+//		String name = "Ivan Petrov";
+		
+		Person person = new Person(8210733456L, "Ivan Petrov", false);
+		Person p2 = new Person(7512733476L, "Dimitar Dimitrov", 29, false,
+				"dimitar@gmail.com", "+3592 3244354");
+//		person.idNumber = 8210733456L;
+//		person.name = "Ivan Petrov";
+//		person.age = 33;
+//		person.female = false;
+//		person.email = "ivan.petrov@abv.bg";
+//		person.phone = "+3592 4444322";
+		
+		System.out.println("Person Id: " + person.getIdNumber()
+			+ ": " + person.getName()
+			+ ", age: " + person.getAge()			
+		);
+		
+		System.out.println(p2);
+		
+		
 	}
 
 }
